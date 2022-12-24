ef33fdfe3d8cc5d61b3399e9cb2784a5618e3d47
Corin Faife <corin.faife@gmail.com>
2022-12-24 14:31:47 -0600

    Update About page to list Cornell Tech

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,15 @@
+
 <template>
   <div class="about">
     <h1>{{ msg }}</h1>
     <h2>Hi.</h2>
     <div class="about-body">
       <p>I'm a journalist interested in technology, politics, and the politics of technology. </p>
-      <p>I think a lot about the incentives baked into social and technological systems,
-        and how these bring the interests of owners, coordinators and end users into conflict.</p>
       <p>After starting my career in London, then moving to Montréal, I'm now based in New York where I attended the
         <a href="https://www.recurse.com/" target="_blank">Recurse Center</a> programming retreat in 2019.</p>
-      <p>Since then I've worked at global nonprofit <a href='https://www.witness.org/'>WITNESS</a> researching deepfakes and AI-driven media manipulation, and at <a href='https://themarkup.org/'>The Markup</a> on the 
-      <a href="https://themarkup.org/series/citizen-browser">Citizen Browser</a> Facebook accountability project.</p>
-      <p>Currently I'm a <a href='https://www.theverge.com/authors/corin-faife'>senior reporter at The Verge</a> covering privacy, surveillance and cybersecurity.</p>
+      <p>Since then I've worked at global nonprofit <a href='https://www.witness.org/'>WITNESS</a> researching deepfakes and media manipulation, at <a href='https://themarkup.org/'>The Markup</a> on the 
+      <a href="https://themarkup.org/series/citizen-browser">Citizen Browser</a> Facebook accountability project, and as a <a href='https://www.theverge.com/authors/corin-faife'>senior reporter at The Verge</a> covering privacy, surveillance and cybersecurity.</p>
+      <p>Currently I'm the inaugural <a href="https://www.dli.tech.cornell.edu/members/Faife">Peggy J. Koenig Civic Innovation Fellow</a> at Cornell Tech university, based at the Digital Life Initiative under the supervision of Professor Helen Nissenbaum.</p>
       <p>Use the info <a href='https://corinfaife.co/#/contact'>here</a> to say hello!</p>
    </div>
 
